c07019249d748ba01aaaa7e5d957b0e8b7f5ffc0
Yury Trembach <yury.trembach@gmail.com>
2023-10-23 02:07:53 +0300

    Add TextCheck

---
+++ src/main/java/org/yt/jr/projects/util/TextCheck.java
@@ -0,0 +1,15 @@
+package org.yt.jr.projects.util;
+
+public class TextCheck {
+
+    final private char[] text;
+
+    public TextCheck(char[] text){
+        this.text = text;
+    }
+
+    public boolean hasSignBetweenLetters(final int segmentLength) {
+        return true;
+    }
+
+}
