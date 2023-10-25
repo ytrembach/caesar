98b0c5810c5ade9e3b5532e8aeb65e6bae45798c
Yury Trembach <yury.trembach@gmail.com>
2023-10-25 17:16:12 +0300

    Add UserInterface abstract class

---
+++ src/main/java/org/yt/jr/projects/ui/UserInterface.java
@@ -0,0 +1,28 @@
+package org.yt.jr.projects.ui;
+
+import org.yt.jr.projects.alphabet.Alphabet;
+import org.yt.jr.projects.alphabet.SupportedAlphabets;
+import org.yt.jr.projects.ui.processors.Processor;
+
+import java.nio.file.Path;
+
+public abstract class UserInterface {
+    protected abstract Control getNextControl();
+
+    public void processControls() {
+        Control nextControl;
+        while ((nextControl = getNextControl()).getCmd() != Commands.EXIT) {
+            Processor processor = nextControl.getCmd().getProcessor();
+            String param = nextControl.getParam();
+            if (processor != null) {
+                System.out.printf("%s: %s%n",nextControl.getCmd().getTitle(), param);
+                processor.process(param);
+            }
+        }
+
+        System.out.println("Bye!");
+    }
+
+    ;
+
+}
