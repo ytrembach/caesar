29e8bcb336b49b85f18f447aa787c01850fbf99e
Yury Trembach <yury.trembach@gmail.com>
2023-10-25 17:16:25 +0300

    Add CmdLine handler

---
+++ src/main/java/org/yt/jr/projects/ui/UserInterfaceCmdline.java
@@ -0,0 +1,29 @@
+package org.yt.jr.projects.ui;
+
+import java.util.ArrayList;
+
+public class UserInterfaceCmdline extends UserInterface {
+
+    ArrayList<Control> queue = new ArrayList<>();
+
+    protected Control getNextControl() {
+        if (!queue.isEmpty()) {
+            return queue.remove(0);
+        }
+        return(Control.EXIT);
+    }
+
+    public UserInterfaceCmdline(String userCmd, String userPath, String userKey) {
+        queue.add(new Control(Commands.SET_PATH, userPath));
+        if (!"0".equals(userKey)) {
+            queue.add(new Control(Commands.SET_KEY, userKey));
+        }
+
+        for (Commands command : Commands.values()) {
+            if (command.getCmd().equals(userCmd.toUpperCase())) {
+                queue.add(new Control(command, ""));
+            }
+        }
+    }
+
+}
