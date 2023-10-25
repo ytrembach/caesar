7ba4eb8df068ab57dd10f8f6f2b182106a82e209
Yury Trembach <yury.trembach@gmail.com>
2023-10-25 17:15:37 +0300

    Add Commands enum

---
+++ src/main/java/org/yt/jr/projects/ui/Commands.java
@@ -0,0 +1,38 @@
+package org.yt.jr.projects.ui;
+
+import org.yt.jr.projects.ui.processors.EncryptProcessor;
+import org.yt.jr.projects.ui.processors.Processor;
+import org.yt.jr.projects.ui.processors.SetKeyProcessor;
+import org.yt.jr.projects.ui.processors.SetPathProcessor;
+
+public enum Commands {
+    EXIT("Exit", null, ""),
+    SET_PATH("Set the path to file to proceed", new SetPathProcessor(), ""),
+    SET_KEY("Set the key", new SetKeyProcessor(), ""),
+    ENCRYPT("Encrypt file", new EncryptProcessor(), "ENCRYPT"),
+    DECRYPT("Decrypt by key", null, "DECRYPT"),
+    BRUTEFORCE_ALT("Decrypt by alternate brute force method", null, "BRUTE_FORCE_ALT"),
+    BRUTEFORCE_FREQ("Decrypt by frequency analysis brute force", null, "BRUTE_FORCE");
+
+    final private String title;
+    final private Processor processor;
+    final private String cmd;
+
+    Commands(final String title, final Processor processor, final String cmd) {
+        this.title = title;
+        this.processor = processor;
+        this.cmd = cmd;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Processor getProcessor() {
+        return processor;
+    }
+
+    public String getCmd() {
+        return cmd;
+    }
+}
