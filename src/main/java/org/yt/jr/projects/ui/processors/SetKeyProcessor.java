bff63cc8b494e4789a383eb0fea38b92193f6b58
Yury Trembach <yury.trembach@gmail.com>
2023-10-26 13:47:12 +0300

    Change Processor to interface

--- src/main/java/org/yt/jr/projects/ui/processors/SetKeyProcessor.java
+++ src/main/java/org/yt/jr/projects/ui/processors/SetKeyProcessor.java
@@ -2,19 +2,19 @@ package org.yt.jr.projects.ui.processors;
 
 import org.yt.jr.projects.ui.Config;
 
+import java.security.InvalidKeyException;
 import java.security.InvalidParameterException;
 
-public class SetKeyProcessor extends Processor {
-    public void process(final String keyStr) {
+public class SetKeyProcessor implements Processor {
+    public int process(final String keyStr) {
         int key;
         try {
             key = Integer.parseInt(keyStr);
+            Config.CONFIG.setKey(key);
+            return 0;
         } catch (NumberFormatException e) {
-            System.out.println("Key should be an integer number");
-            throw new InvalidParameterException();
+            System.out.print("Key should be an integer number: ");
         }
-
-        Config.CONFIG.setKey(key);
-
+        return -1;
     }
 }
--- src/main/java/org/yt/jr/projects/ui/processors/SetPathProcessor.java
+++ src/main/java/org/yt/jr/projects/ui/processors/SetPathProcessor.java
@@ -5,13 +5,15 @@ import org.yt.jr.projects.ui.Config;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class SetPathProcessor extends Processor {
-    public void process(final String fileName) {
+public class SetPathProcessor implements Processor {
+    public int process(final String fileName) {
         Path path = Path.of(fileName);
         if (Files.exists(path)) {
             Config.CONFIG.setPath(path);
+            return 0;
         } else {
-            System.out.println("File not found");
+            System.out.printf("File not found: %s: ", fileName);
+            return -1;
         }
     }
 }
