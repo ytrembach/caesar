13ae4c606ab1e74222ed5c32fc76701204219873
Yury Trembach <yury.trembach@gmail.com>
2023-10-26 17:14:58 +0300

    Rename CryptProcess to GeneralCryptProcessor

--- src/main/java/org/yt/jr/projects/ui/processors/DecryptProcessor.java
+++ src/main/java/org/yt/jr/projects/ui/processors/DecryptProcessor.java
@@ -2,7 +2,7 @@ package org.yt.jr.projects.ui.processors;
 
 import org.yt.jr.projects.ui.Config;
 
-public class DecryptProcessor extends CryptProcess implements Processor {
+public class DecryptProcessor extends GeneralCryptProcessor implements Processor {
     public int process(final String keyStr) {
         if (!prepare("[DECRYPTED]")) {
             return -1;
